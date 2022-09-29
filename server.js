7553437bff89f1b1edf19085d7cccfde67ca26e8
Ari Wisnu <63925882+dewisnu@users.noreply.github.com>
2022-09-30 04:20:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const http = require("http")
 
+
+const server = http.createServer((req,res) => {
+    console.log("new connection")
+    res.end("We can use GCP")
+});
+
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}`)
+})
